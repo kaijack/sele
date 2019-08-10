363748ad8c4679f6090c7bb9a5eb235b53adf663
kaijack <jakkse7en@gmail.com>
2019-08-10 12:11:06 -0400

    update navbar ke tengah

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
+  <div class="card">
     <b-navbar toggleable="lg" type="light" style="background-color: white;" class="fixed-top">
-      <b-navbar-toggle target="nav-collapse">
-        <h5>Selena</h5>
-      </b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <div class="container">
+        <b-navbar-toggle target="nav-collapse">
+          <h5>Selena</h5>
+        </b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
-        <b-navbar-nav>
-          <a-input placeholder="Cari aktifitas" class="sos">
-            <a-icon slot="prefix" type="search" />
-            <a-icon v-if="userName" slot="suffix" type="close-circle" @click="emitEmpty" />
-          </a-input>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-
-        <b-navbar-nav class="ml-auto">
           <b-navbar-nav>
-            <b-nav-item href="#">About</b-nav-item>
-            <b-nav-item href="#">Blog</b-nav-item>
-            <b-nav-item href="#">Bantuan</b-nav-item>
-            <popsign />
+            <a-input placeholder="Cari aktifitas" class="sos">
+              <a-icon slot="prefix" type="search" />
+              <a-icon v-if="userName" slot="suffix" type="close-circle" @click="emitEmpty" />
+            </a-input>
           </b-navbar-nav>
-        </b-navbar-nav>
-      </b-collapse>
+
+          <!-- Right aligned nav items -->
+
+          <b-navbar-nav class="ml-auto">
+            <b-navbar-nav>
+              <b-nav-item href="#">About</b-nav-item>
+              <b-nav-item href="#">Blog</b-nav-item>
+              <b-nav-item href="#">Bantuan</b-nav-item>
+              <popsign />
+            </b-navbar-nav>
+          </b-navbar-nav>
+        </b-collapse>
+      </div>
     </b-navbar>
   </div>
 </template>
