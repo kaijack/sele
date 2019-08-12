601e17d3e5e7dedb36e9bebfd23e87ac8e546f36
kaijack <jakkse7en@gmail.com>
2019-08-12 04:25:24 -0400

    update card

--- src/components/Cardfix.vue
+++ src/components/Cardfix.vue
@@ -12,35 +12,31 @@
       <b>Tanggal Tersedia</b>
       <br />
       <br />
-      <a-dropdown>
-        <a-menu slot="overlay" @click="handleMenuClick">
-          <a-menu-item key="1">Sabtu, 14 Juni - 15.00</a-menu-item>
-          <a-menu-item key="2">Sabtu, 28 Juni - 15.00</a-menu-item>
-          <a-menu-item key="3">Sabtu, 03 Agustus - 15.00</a-menu-item>
-        </a-menu>
-        <a-button block>
-          <a style="float: left">Sabtu, 21 Juni - 15.00</a>
-          <a-icon type="down" style="float: right;" />
-        </a-button>
-      </a-dropdown>
+      <a-select defaultValue="Sabtu, 21 Agustus 2019" style="width: 245px" @change="handleChange">
+        <a-select-option value="Senin, 25 Agustus 2019">Senin, 25 Agustus 2019</a-select-option>
+        <a-select-option value="Rabu, 27 Agustus 2019">Rabu, 27 Agustus 2019</a-select-option>
+        <a-select-option value="Jumat, 29 Agustus 2019">Jumat, 29 Agustus 2019</a-select-option>
+      </a-select>
       <br />
     </div>
     <div>
       <br />
+      <br />
       <b>Jumlah yang ikut</b>
       <br />
+      <br />
 
-      <div class="container">
+      <div>
         <div class="row">
           <div class="col-5">
             <a-button style="text-align: left; width: 130px; ">2 orang</a-button>&nbsp;
           </div>
-          <div class="col-2" style="margin-left: 40px;">
+          <div class="col-2" style="margin-left: 10px;">
             <a-button style="background: #D9D9E3;">
               <a-icon type="minus" style="padding-bottom: 6px; color: black;" />
             </a-button>
-          </div>&nbsp;
-          <div class="col-2" style="margin-left: 10px;">
+          </div>
+          <div class="col-2">
             <a-button style="background: #FFA500;">
               <a-icon type="plus" style="padding-bottom: 6px; color: black;" />
             </a-button>
@@ -85,7 +81,7 @@ export default {
   border-radius: 4px;
   background-color: #fff;
   box-shadow: 0 5px 18px 0 rgba(227, 123, 40, 0.1);
-  padding: 50px;
+  padding: 27px;
   margin-bottom: 75px;
 }
 </style>
